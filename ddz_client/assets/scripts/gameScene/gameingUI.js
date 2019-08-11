8c4643ace1bac3f3ecc29b27f0edc3722a5a7685
shuyi <shuyi@kingsoft.com>
2019-08-11 10:09:53 +0800

    add:发牌动画

--- ddz_client/assets/scripts/gameScene/gameingUI.js
+++ ddz_client/assets/scripts/gameScene/gameingUI.js
@@ -11,6 +11,7 @@ cc.Class({
         myglobal.socket.onPushCards(function(data){
             console.log("onPushCards"+JSON.stringify(data))
             this.pushCard(data)
+            //左边移动
             this.node.parent.emit("pushcard_other_event")
             //console.log("parent.name"+this.node.parent.name)
            
@@ -39,15 +40,23 @@ cc.Class({
             });
         }
       //创建card预制体
+      var cards_nods = []
       for(var i=0;i<17;i++){
+        
         var card = cc.instantiate(this.card_prefab)
         card.scale=0.8
-        console.log("gameUI this.card_node.parent"+ this.node.parent.name)
+        //console.log("gameUI this.card_node.parent"+ this.node.parent.name)
         card.parent = this.node.parent
-        card.x = card.width * 0.4 * (17 - 1) * -0.5 + card.width * 0.4 * i;
-        card.y = -250
+      
+        //card.x = card.width * 0.4 * (17 - 1) * (-0.5) + card.width * 0.4 * i;
+        //card.y = -250
+        var newx = card.width * 0.4 * (17 - 1) * (-0.5) + card.width * 0.4 * i;
+        var newy = -250
+        var action = cc.moveTo(1, cc.v2(newx, newy));
+        card.runAction(action)
         //console.log("card_prefab position"+card.position)
         card.getComponent("card").showCards(data[i])
+        
       }
 
     }
